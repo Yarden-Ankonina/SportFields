7e347cd9f7c2dd50a3549970d2f830b56c6747b4
Yarden Ankonina <yardi1994@gmail.com>
2022-06-22 10:01:27 +0300

    About

---
+++ client/js/about.js
@@ -0,0 +1,24 @@
+let navbarAbout = document.getElementsByClassName('navbar-about')[0]
+
+navbarAbout.addEventListener('click',()=>{
+    let aboutModal = createAboutModal("SportFields",
+    "Sportfields is an app that helps you find sport fields with a map interface.<br/><br/>You can add new fields that are not on the map using the 'New Field' button.<br/>You can select fields and see their details,<br/>after a field is selected you can select how many people are there in the field.<br/>There are 3 levels of population : Crowded, Populated, Empty.<br/>You can add a field to your liked fields by pressing the heart icon.<br/>Press the 'Likes' button to see what fields you have in your liked list.<br/><br/>Thank you for using SportFields."
+    )
+    modal(aboutModal.id)
+    
+})
+
+function createAboutModal(text,subText){
+    let modalAbout = document.createElement('div')
+    let subTextModal = document.createElement('div')
+    subTextModal.innerHTML = subText
+    modalAbout.className = "modal on modal-about"
+    modalAbout.setAttribute('id','modalAbout')
+    modalAbout.innerHTML = text + '<br><br>'
+    modalAbout.style.fontWeight = 'bold'
+    subTextModal.style.fontWeight = 'normal'
+    subTextModal.style.textAlign = 'left'
+    modalAbout.appendChild(subTextModal)
+    document.body.appendChild(modalAbout)
+    return modalAbout
+}
