45bd957e3d7f6d524f0b4a57a482e158c719d939
Yarden Ankonina <yardi1994@gmail.com>
2022-06-23 06:43:13 +0300

    about update

--- client/js/about.js
+++ client/js/about.js
@@ -1,9 +1,23 @@
 let navbarAbout = document.getElementsByClassName('navbar-about')[0]
+let aboutString = `
+The SportFields app has a geographical map interface to help you easily find available sports facilities to play various sports near you.<br/> 
 
+Click on any existing field on the map, to see more about it: <br/>
+
+•	See its location, size, operational status, contact person, and other details.<br/> 
+•	See its current availability  for play, or report an update on its availability by selecting Crowded, Populated, or Empty.<br/>
+•	Click on the ♥ icon to add the field to your favorite “liked” list and mark the field in blue on the map. <br/>
+ 
+Click on the 'New Field' button to add new playing fields, sports grounds, pitches, swimming pools, etc. that are not on the map.<br/>
+
+Click on the ‘Likes ♥’ button to see your selected list of liked fields.<br/>
+
+
+
+Thank you for using SportFields. Have a great game!
+`
 navbarAbout.addEventListener('click',()=>{
-    let aboutModal = createAboutModal("SportFields",
-    "Sportfields is an app that helps you find sport fields with a map interface.<br/><br/>You can add new fields that are not on the map using the 'New Field' button.<br/>You can select fields and see their details,<br/>after a field is selected you can select how many people are there in the field.<br/>There are 3 levels of population : Crowded, Populated, Empty.<br/>You can add a field to your liked fields by pressing the heart icon.<br/>Press the 'Likes' button to see what fields you have in your liked list.<br/><br/>Thank you for using SportFields."
-    )
+    let aboutModal = createAboutModal("SportFields",aboutString)
     modal(aboutModal.id)
     
 })
